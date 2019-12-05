d66104809cc5c3f002906e78b1eb3bd3397de4ac
Mikhail Viryasov <viryasov.mikhail@gmail.com>
2019-12-05 20:28:02 +0300

    Added message sending from last to first

--- main.c
+++ main.c
@@ -22,14 +22,27 @@ int main(int argc, char* argv[]) {
 			process_rank);
 		printf("Message in process %d from process %d: %s\n",
 			process_rank, status.MPI_SOURCE, message);
+		printf("Start message sending from process %d\n", process_rank);
+		MPI_Send(message, strlen(message), MPI_CHAR,
+			(process_rank == (process_count - 1)) ? 0 : process_rank + 1,
+			TAG, MPI_COMM_WORLD);
+		printf("End message sending from process %d\n", process_rank);
 	}
-	if (process_rank + 1 < process_count) {
-		if (process_rank == 0)
-			strcpy(message, "hello from process 0");
+	else {
+		strcpy(message, "hello from process 0");
 		printf("Start message sending from process %d\n", process_rank);
 		MPI_Send(message, strlen(message), MPI_CHAR,
 			process_rank + 1, TAG, MPI_COMM_WORLD);
 		printf("End message sending from process %d\n", process_rank);
+		MPI_Status status;
+		printf("Start message receiving in process %d\n", process_rank);
+		MPI_Recv(message, MAX_MESSAGE_BUFFER_SIZE, MPI_CHAR,
+			process_count - 1, TAG,
+			MPI_COMM_WORLD, &status);
+		printf("End message receiving in process %d\n",
+			process_rank);
+		printf("Message in process %d from process %d: %s\n",
+			process_rank, status.MPI_SOURCE, message);
 	}
 	MPI_Finalize();
 }
